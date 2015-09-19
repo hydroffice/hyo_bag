fb891a80a7f1922ddf5aaff7c0d374060bdccc03
gmasetti <none@none>
2015-09-18 22:11:05 -0400

    First commit

---
+++ hydroffice/__init__.py
@@ -0,0 +1,5 @@
+"""
+HydrOffice Namespace Packages
+Nothing else to put here since it will not be installed!
+"""
+__import__('pkg_resources').declare_namespace(__name__)
---
+++ hydroffice/bag/__init__.py
@@ -0,0 +1,16 @@
+"""
+Hydro-Package
+bag
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__version__ = '0.1.3'
+__doc__ = 'bag'
+__author__ = 'gmasetti@ccom.unh.edu'
+__license__ = 'BSD-like license'
+__copyright__ = 'Copyright 2015 Giuseppe Masetti'
+
+
+def hyo():
+    return __doc__, __version__
