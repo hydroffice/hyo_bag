995dc46a8b6d6a0cbbaa91d9d681555e4c445245
giumas <gmasetti@ccom.unh.edu>
2016-04-24 22:23:40 -0400

    Bump to version 0.2.11

--- hydroffice/bag/__init__.py
+++ hydroffice/bag/__init__.py
@@ -14,7 +14,7 @@ from .base import is_bag
 from .bag import BAGFile
 
 
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 __doc__ = 'BAG'
 __author__ = 'gmasetti@ccom.unh.edu, brc@ccom.unh.edu'
 __license__ = 'LGPLv3 license'
