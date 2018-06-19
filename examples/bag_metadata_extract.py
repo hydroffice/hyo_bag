81eecb2b6a4254413c8fa00ea8e346cd82e54499
giumas <giumas@yahoo.it>
2018-06-19 14:12:59 +0200

    attempt to add support to read old BAG metadata

--- examples/bag_metadata_extract.py
+++ examples/bag_metadata_extract.py
@@ -12,14 +12,18 @@ logger.addHandler(ch)
 from hyo.bag import BAGFile
 from hyo.bag import BAGError
 from hyo.bag.helper import Helper
+from hyo.bag.meta import Meta
 
 # file_bag_0 = os.path.join(Helper.samples_folder(), "bdb_01.bag")
-file_bag_0 = os.path.join(os.path.dirname(__file__), "tmp_copy.bag")
+# file_bag_0 = os.path.join(os.path.dirname(__file__), "tmp_copy.bag")
+file_bag_0 = R"C:\code\hyo\bagexplorer\bag_tools\H11709_1m_MLLW_5of24.bag"
 if os.path.exists(file_bag_0):
     print("- file_bag_0: %s" % file_bag_0)
 
 bag_0 = BAGFile(file_bag_0)
 print(bag_0)
+
+meta = Meta(bag_0.metadata())
 
 output_xml = "original_metadata.xml"
 bag_0.extract_metadata(output_xml)
--- hyo/bag/meta.py
+++ hyo/bag/meta.py
@@ -5,12 +5,12 @@ import numpy as np
 import h5py
 from lxml import etree
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 from .helper import Helper
 
 
-class Meta(object):
+class Meta:
     """ Helper class to manage BAG xml metadata. """
 
     ns = {
@@ -20,6 +20,12 @@ class Meta(object):
         'gmi': 'http://www.isotc211.org/2005/gmi',
         'gml': 'http://www.opengis.net/gml/3.2',
         'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+    }
+
+    ns2 = {
+        'gml': 'http://www.opengis.net/gml',
+        'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        'smXML': 'http://metadata.dgiwg.org/smXML',
     }
 
     def __init__(self, meta_xml):
@@ -116,15 +122,25 @@ class Meta(object):
                                       'gmd:axisDimensionProperties/gmd:MD_Dimension/gmd:dimensionSize/gco:Integer',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read rows and cols: %s" % e)
+            logger.warning("unable to read rows and cols: %s" % e)
             return
+
+        if len(ret) == 0:
+
+            try:
+                ret = self.xml_tree.xpath('//*/spatialRepresentationInfo/smXML:MD_Georectified/'
+                                          'axisDimensionProperties/smXML:MD_Dimension/dimensionSize',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read rows and cols: %s" % e)
+                return
 
         try:
             self.rows = int(ret[0].text)
             self.cols = int(ret[1].text)
 
         except (ValueError, IndexError) as e:
-            log.warning("unable to read rows and cols: %s" % e)
+            logger.warning("unable to read rows and cols: %s" % e)
             return
 
     def _read_res_x_and_y(self):
@@ -135,15 +151,26 @@ class Meta(object):
                                       'gmd:axisDimensionProperties/gmd:MD_Dimension/gmd:resolution/gco:Measure',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read res x and y: %s" % e)
+            logger.warning("unable to read res x and y: %s" % e)
             return
+
+        if len(ret) == 0:
+
+            try:
+                ret = self.xml_tree.xpath('//*/spatialRepresentationInfo/smXML:MD_Georectified/'
+                                          'axisDimensionProperties/smXML:MD_Dimension/resolution/'
+                                          'smXML:Measure/smXML:value',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read res x and y: %s" % e)
+                return
 
         try:
             self.res_x = float(ret[0].text)
             self.res_y = float(ret[1].text)
 
         except (ValueError, IndexError) as e:
-            log.warning("unable to read res x and y: %s" % e)
+            logger.warning("unable to read res x and y: %s" % e)
             return
 
     def _read_corners_sw_and_ne(self):
@@ -153,16 +180,21 @@ class Meta(object):
             ret = self.xml_tree.xpath('//*/gmd:spatialRepresentationInfo/gmd:MD_Georectified/'
                                       'gmd:cornerPoints/gml:Point/gml:coordinates',
                                       namespaces=self.ns)[0].text.split()
-        except etree.Error as e:
-            log.warning("unable to read corners SW and NE: %s" % e)
-            return
+        except (etree.Error, IndexError) as e:
+            try:
+                ret = self.xml_tree.xpath('//*/spatialRepresentationInfo/smXML:MD_Georectified/'
+                                          'cornerPoints/gml:Point/gml:coordinates',
+                                          namespaces=self.ns2)[0].text.split()
+            except (etree.Error, IndexError) as e:
+                logger.warning("unable to read corners SW and NE: %s" % e)
+                return
 
         try:
             self.sw = [float(c) for c in ret[0].split(',')]
             self.ne = [float(c) for c in ret[1].split(',')]
 
         except (ValueError, IndexError) as e:
-            log.warning("unable to read corners SW and NE: %s" % e)
+            logger.warning("unable to read corners SW and NE: %s" % e)
             return
 
     def _read_wkt_prj(self):
@@ -173,14 +205,26 @@ class Meta(object):
                                       'gmd:referenceSystemIdentifier/gmd:RS_Identifier/gmd:code/gco:CharacterString',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the WKT projection string: %s" % e)
+            logger.warning("unable to read the WKT projection string: %s" % e)
             return
+
+        if len(ret) == 0:
+            try:
+                ret = self.xml_tree.xpath('//*/referenceSystemInfo/smXML:MD_CRS',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the WKT projection string: %s" % e)
+                return
+
+            if len(ret) != 0:
+                logger.warning("unsupported method to describe CRS")
+                return
 
         try:
             self.wkt_srs = ret[0].text
 
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the WKT projection string: %s" % e)
+            logger.warning("unable to read the WKT projection string: %s" % e)
             return
 
     def _read_bbox(self):
@@ -192,14 +236,24 @@ class Meta(object):
             ret_x_max = self.xml_tree.xpath('//*/gmd:EX_GeographicBoundingBox/gmd:eastBoundLongitude/gco:Decimal',
                                             namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the bbox's longitude values: %s" % e)
+            logger.warning("unable to read the bbox's longitude values: %s" % e)
             return
+
+        if len(ret_x_min) == 0:
+            try:
+                ret_x_min = self.xml_tree.xpath('//*/smXML:EX_GeographicBoundingBox/westBoundLongitude',
+                                                namespaces=self.ns2)
+                ret_x_max = self.xml_tree.xpath('//*/smXML:EX_GeographicBoundingBox/eastBoundLongitude',
+                                                namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the bbox's longitude values: %s" % e)
+                return
 
         try:
             self.lon_min = float(ret_x_min[0].text)
             self.lon_max = float(ret_x_max[0].text)
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the bbox's longitude values: %s" % e)
+            logger.warning("unable to read the bbox's longitude values: %s" % e)
             return
 
         try:
@@ -208,14 +262,25 @@ class Meta(object):
             ret_y_max = self.xml_tree.xpath('//*/gmd:EX_GeographicBoundingBox/gmd:northBoundLatitude/gco:Decimal',
                                             namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the bbox's latitude values: %s" % e)
+            logger.warning("unable to read the bbox's latitude values: %s" % e)
             return
+
+        if len(ret_y_min) == 0:
+
+            try:
+                ret_y_min = self.xml_tree.xpath('//*/smXML:EX_GeographicBoundingBox/southBoundLatitude',
+                                                namespaces=self.ns2)
+                ret_y_max = self.xml_tree.xpath('//*/smXML:EX_GeographicBoundingBox/northBoundLatitude',
+                                                namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the bbox's latitude values: %s" % e)
+                return
 
         try:
             self.lat_min = float(ret_y_min[0].text)
             self.lat_max = float(ret_y_max[0].text)
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the bbox's latitude values: %s" % e)
+            logger.warning("unable to read the bbox's latitude values: %s" % e)
             return
 
     def _read_abstract(self):
@@ -225,13 +290,21 @@ class Meta(object):
             ret = self.xml_tree.xpath('//*/gmd:abstract/gco:CharacterString',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the abstract string: %s" % e)
+            logger.warning("unable to read the abstract string: %s" % e)
             return
+
+        if len(ret) == 0:
+            try:
+                ret = self.xml_tree.xpath('//*/abstract',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the abstract string: %s" % e)
+                return
 
         try:
             self.abstract = ret[0].text
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the abstract string: %s" % e)
+            logger.warning("unable to read the abstract string: %s" % e)
             return
 
     def _read_date(self):
@@ -241,13 +314,21 @@ class Meta(object):
             ret = self.xml_tree.xpath('//*/gmd:CI_Date/gmd:date/gco:Date',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the date string: %s" % e)
+            logger.warning("unable to read the date string: %s" % e)
             return
+
+        if len(ret) == 0:
+            try:
+                ret = self.xml_tree.xpath('//*/smXML:CI_Date/date',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the date string: %s" % e)
+                return
 
         try:
             text_date = ret[0].text
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the date string: %s" % e)
+            logger.warning("unable to read the date string: %s" % e)
             return
 
         tm_date = None
@@ -256,7 +337,7 @@ class Meta(object):
             parsed_date = dateutil.parser.parse(text_date)
             tm_date = parsed_date.strftime('%Y-%m-%dT%H:%M:%SZ')
         except Exception:
-            log.warning("unable to handle the date string: %s" % text_date)
+            logger.warning("unable to handle the date string: %s" % text_date)
 
         if tm_date is None:
             self.date = text_date
@@ -270,11 +351,20 @@ class Meta(object):
             ret = self.xml_tree.xpath('//*/bag:verticalUncertaintyType/bag:BAG_VertUncertCode/@codeListValue',
                                       namespaces=self.ns)
         except etree.Error as e:
-            log.warning("unable to read the uncertainty type string: %s" % e)
+            logger.warning("unable to read the uncertainty type string: %s" % e)
             return
+
+        if len(ret) == 0:
+
+            try:
+                ret = self.xml_tree.xpath('//*/verticalUncertaintyType',
+                                          namespaces=self.ns2)
+            except etree.Error as e:
+                logger.warning("unable to read the uncertainty type string: %s" % e)
+                return
 
         try:
             self.unc_type = ret[0]
         except (ValueError, IndexError) as e:
-            log.warning("unable to read the uncertainty type attribute: %s" % e)
+            logger.warning("unable to read the uncertainty type attribute: %s" % e)
             return
